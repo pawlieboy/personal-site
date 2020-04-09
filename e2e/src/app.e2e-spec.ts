788418352678e1f01f6a510da91deac6f8030966
Paul Berges <paulberges24@gmail.com>
2020-04-09 18:21:10 -0400

    Updated tests

--- e2e/src/app.e2e-spec.ts
+++ e2e/src/app.e2e-spec.ts
@@ -10,7 +10,7 @@ describe('workspace-project App', () => {
 
   it('should display welcome message', () => {
     page.navigateTo();
-    expect(page.getTitleText()).toEqual('personal-site app is running!');
+    expect(page.getTitleText()).toEqual('Hi, I\'m Paul Berges');
   });
 
   afterEach(async () => {
--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -16,16 +16,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'personal-site'`, () => {
+  it(`should have as title 'Hi, I\'m Paul Berges'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('personal-site');
+    expect(app.title).toEqual('Hi, I\'m Paul Berges');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement;
-    expect(compiled.querySelector('.content span').textContent).toContain('personal-site app is running!');
+    expect(compiled.querySelector('h1').textContent).toContain('Hi, I\'m Paul Berges');
   });
 });
